dbbc06dab97f2bb4fc48d597ec82e4366fc66459
athulcoder <athul.sin.cos.tan@gmail.com>
2025-01-30 23:41:32 +0530

    framer-motion added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,16 @@
+import { motion } from "framer-motion";
+
 /* eslint-disable react/prop-types */
 const Button = ({ label }) => {
   return (
-    <button className="font-montserrat text-[15x] text-center text-white  bg-accent rounded-xl py-3 px-5 mt-4 hover:bg-accent-hover ">
+    <motion.button
+      whileTap={{ scale: 1.1 }}
+      whileHover={{ scale: 1.1, rotate: 90, backgroundColor: "#fff" }}
+      transition={{ bounceStiffness: 600, bounceDamping: 10 }}
+      className="font-montserrat text-[15x] text-center text-white  bg-accent rounded-xl py-3 px-5 mt-4 hover:bg-accent-hover "
+    >
       {label}
-    </button>
+    </motion.button>
   );
 };
 
--- src/components/ThemeModeButton.jsx
+++ src/components/ThemeModeButton.jsx
@@ -1,7 +1,7 @@
 import { MdDarkMode } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
-import { useState, useEffect } from "react";
-import "../index.css";
+import { useEffect } from "react";
+import { motion } from "framer-motion";
 import { useLocalStorage } from "react-use";
 const ThemeModeButton = () => {
   // use localStorage to store the theme and will retrive if page is reloaded.
@@ -22,12 +22,15 @@ const ThemeModeButton = () => {
   };
   return dark ? (
     <div>
-      <button
+      <motion.button
         className="bg-primary-color p-2 rounded-full"
         onClick={handleClick}
+        whileTap={{ scale: 1.1 }}
+        whileHover={{ scale: 1.1, rotate: 90, backgroundColor: "#fff" }}
+        transition={{ bounceStiffness: 600, bounceDamping: 10 }}
       >
         <MdDarkMode className="w-[24px] h-[24px]  text-secondary-text"></MdDarkMode>
-      </button>
+      </motion.button>
     </div>
   ) : (
     <div>
