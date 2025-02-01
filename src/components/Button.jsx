021fb2428f3aa1adcf46a24fe60183f4b56f1cd2
athulcoder <athul.sin.cos.tan@gmail.com>
2025-02-01 12:55:08 +0530

    update button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ label }) => {
   return (
     <motion.button
       whileTap={{ scale: 1.1 }}
-      whileHover={{ scale: 1.1, backgroundColor: "#fff", color: "#000" }}
+      whileHover={{ scale: 1.1, backgroundColor: "#faf8fa", color: "#000" }}
       transition={{ bounceStiffness: 600, bounceDamping: 10 }}
       className="font-montserrat text-[15x] text-center text-white  bg-accent rounded-xl py-3 px-5 mt-4 hover:bg-accent-hover "
     >
