cc5425d58a1ff8ea77161456c87674906c9ea0b0
athulcoder <athul.sin.cos.tan@gmail.com>
2025-02-02 18:28:28 +0530

    new project added

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,6 +3,7 @@ import HeroIMG from "./h5.avif"
 import P1IMG from "./p1.webp";
 import P2IMG from "./p2.webp";
 import P3IMG from "./p3.webp";
+import P4IMG from "./p4.webp";
 
 
 
@@ -10,6 +11,7 @@ export{
     HeroIMG,
     P1IMG,
     P3IMG,
+    P4IMG,
     P2IMG
 
 }
