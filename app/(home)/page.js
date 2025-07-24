fea46800ffbc366e1cfca414099f12a14395bd59
athulcoder <athul.sin.cos.tan@gmail.com>
2025-07-24 21:33:33 +0530

    portfolio background

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -8,7 +8,7 @@ function BackgroundBeamsDemo() {
     <div className="h-[100vh] w-full  bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-          Join the waitlist
+          ATHUL SABU
         </h1>
         <p></p>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
